6eb740bd67b0cd4b4efd558ce426971f88ac622f
Prinsu Kumar <somilsingh597@gmail.com>
2025-12-16 21:01:51 +0530

    Update axiosClient.js

--- frontend/src/utils/axiosClient.js
+++ frontend/src/utils/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const axiosClient =  axios.create({
-    baseURL: import.meta.env.VITE_API_URL || 'https://one4dev-backend.onrender.com',
+    baseURL:'https://one4dev-backend.onrender.com',
     withCredentials: true,
     headers: {
         'Content-Type': 'application/json'
